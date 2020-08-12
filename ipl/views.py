bf1b857312ea8cf63c76740643b8a09afde20c37
pramit <parmitkumarpkr@gmail.com>
2020-08-12 23:28:45 +0530

    app

--- ipl/views.py
+++ ipl/views.py
@@ -23,8 +23,16 @@ def predict(request):
         runs=request.POST['Runs']
         wickets=request.POST['Wickets']
         last4=request.POST['Runs_4over']
+
         teams=['Chennai Super Kings', 'Delhi Capitals', 'Kings XI Punjab','Kolkata knight Riders',
                 'Mumbai Indians','Rajasthan Royals','Royal Challengers Bangalore','Sunrisers Hyderabad']
+        token=['b67caff6-7524-42cb-874e-b5cef45d6088','8a441173-ce0f-42fb-89e3-5e5ccfde4839',
+                '670c159a-0c22-448a-b064-90adab97e5cb','34c111f9-3794-42eb-b446-112a96e1e660',
+                '50aee1e6-6032-4195-91c2-8d6d72b7b598','41fea1a5-652a-4b1e-9c5d-820770e226be',
+                'e414232d-2447-46b5-b1b0-3be72986ed7b','b0ecb2e3-7f61-4a84-baec-1df742d2e702']
+        token_bat=token[teams.index(bat_team)]
+        token_bowl=token[teams.index(bowl_team)]
+        
         tmp=[]
         f1=[0.0]*8
         f1[teams.index(bat_team)]=1.0
@@ -37,6 +45,7 @@ def predict(request):
         pred1 = int(lasso1.predict(data)[0])
         if(pred1<int(runs)):
             pred1=int(runs)+3
-        return render(request,'ipl.html', {'bat_team':bat_team,'bowl_team':bowl_team,'total':str(pred1)})
+        return render(request,'ipl.html', {'bat_team':bat_team,'bowl_team':bowl_team,'total':str(pred1),'bat_token':token_bat,
+                                'bowl_token':token_bowl})
 
 
--- laliga/views.py
+++ laliga/views.py
@@ -24,6 +24,20 @@ def predict(request):
         team=['Alaves', 'Ath Bilbao', 'Ath Madrid','Barcelona', 'Betis','Celta','Eibar', 'Espanol', 'Getafe',
                 'Granada','Leganes', 'Levante','Mallorca','Osasuna','Real Madrid', 'Sevilla',
                 'Sociedad', 'Valencia', 'Valladolid', 'Villareal']
+
+        token=['703d8acf-c342-44f4-96aa-80ffb2174533','4d48d621-1fd3-4210-a80f-5de0508a4184',
+                'f79e72ae-a0ab-4275-bd68-20413612ee49','bab0c7fb-dae7-41aa-b333-192b59067ee3',
+                '667b3d95-12f1-43c9-9f68-fd1ddbdf6c38','fac65d1e-2914-42b5-8107-c0e4e7d68ded',
+                'cff9918a-cc73-44c1-aba3-80d32407abe9','4840a424-a23a-41f7-81a3-0ca5e1638c23',
+                '52fbf666-ee2c-48ca-80a8-71284a9e6f19','cbf6046b-fc2f-4a78-a042-ce7650529109',
+                '1190c779-cc5d-4e59-8317-706536a34879','52cbba22-edaf-436c-9590-4fb6ba411929',
+                '2579c39e-8603-463e-962f-300e6807e72e','961cd48d-22ea-4395-bcf3-467fe1f7b77e',
+                '0dac7291-70c5-455f-8876-bdba573db77a','ef7ad438-0a7f-4c02-929a-4bdb0e8025dc',
+                'b25ec2c9-1953-48d3-8534-d9a98760ce59','d64a7c00-fba3-4496-b42f-6b525a13c15d',
+                'c5b9588f-7577-4d4f-863f-1e4e04a69f32','baac8391-aee4-4c1c-a6a1-a8601f1dba7e']
+        token_home=token[team.index(home_team)]
+        token_away=token[team.index(away_team)]
+
         tmp=[]
         f1=[0.0]*20
         f1[team.index(home_team)]=1.0
@@ -35,4 +49,5 @@ def predict(request):
         data = np.array([tmp])
         pred1 = int(ridge1.predict(data)[0])
         pred2=  int(ridge2.predict(data)[0])
-        return render(request,'laliga.html', {'home':home_team,'away':away_team,'home_goal':str(pred1),'away_goal':str(pred2)})
+        return render(request,'laliga.html', {'home':home_team,'away':away_team,'home_goal':str(pred1),'away_goal':str(pred2),
+                                                'home_token':token_home,'away_token':token_away})
